b8e4db6b485054a48aff4733830a7c680bbc9206
Chasmyr <lestratgabin5886@gmail.com>
2023-01-08 15:32:50 +0100

    redner with ingredients

--- script/card.js
+++ script/card.js
@@ -3,19 +3,27 @@ const cardWrapper = document.querySelector('.card-wrapper')
 const input = document.querySelector('.search-bar input')
 const ingredientBody = document.querySelector('#collapseOne > .accordion-body')
 const accordionIngr = document.getElementById('accordionOne')
+const tags = document.querySelector('.tags')
 
-// initialiser le contenu avec les données
-//plutot que de faire une simple span, setup une grid css pour générer le contenu
+let filter = {
+    name: '',
+    ingredients: []
+}
+let result = sortRecipesWithArrayMethod(recipes, filter)
+render(result)
+
+// initialiser le contenu avec les données et render avec le filtre
 listeIngredient.map(e => {
     let span = document.createElement('span')
     span.innerText = e
+    span.className = "col-4"
+    span.addEventListener('click', () => {
+        filter.ingredients.push(e)
+        render(sortRecipesWithArrayMethod(recipes, filter))
+    })
 
     ingredientBody.appendChild(span)
 })
-
-let filter = {}
-let result = sortRecipesWithArrayMethod(recipes, filter)
-render(result)
 
 // event listener
 input.addEventListener('keydown', (e) => {
@@ -32,14 +40,13 @@ input.addEventListener('keydown', (e) => {
     }
 })
 accordionIngr.addEventListener('click', () => {
-    if(accordionIngr.querySelector('.accordion-button').classList.contains('collapsed')) {
-        accordionIngr.style.width = '150px'
-        console.log('fermé')
-    } else if (accordionIngr.querySelector('.accordion-collapse').classList.contains('collapsing')){   
-        accordionIngr.style.width = '650px'
-        console.log('collapsing')
-    }
+    
 })
+
+// fn pour créer des tags
+function createTags(filter) {
+    
+}
 
 // fn pour rendre le contenu dynamique
 function render(result) {
@@ -49,7 +56,7 @@ function render(result) {
     } else {
         result.map(recette => {
             let div = document.createElement('div')
-            div.className = 'card col-4'
+            div.className = 'card col-4 p-0 bg-light'
         
             let list = document.createElement('ul')
             list.style.listStyleType = 'none'
@@ -83,13 +90,13 @@ function render(result) {
             div.innerHTML = `<img src="http://via.placeholder.com/380x178/1f1a38/ffffff?text=Image" class="card-img-top" alt="...">` +
             `<div class="card-body row mb-2">` +
                 `<div class="card-title d-flex justify-content-between">` +
-                    `<h5>${recette.name}</h5>` +
-                    `<h5><i class="fa-regular fa-clock ml-6"></i><strong>${recette.time}min</strong></h5>` +
+                    `<h5 style="max-width: 250px;">${recette.name}</h5>` +
+                    `<h5><i class="fa-regular fa-clock me-1"></i><strong>${recette.time}min</strong></h5>` +
                 `</div>` +
                 `<div class="col-6 list-ingr">` +
                 `</div>` +
                 `<div class="col-6 nopadding" style="font-size: 12px;">` +
-                    `<p>${recette.description}</p>` +
+                    `<p class="overflow-hidden" style="max-height: 110px;">${recette.description}</p>` +
                 `</div>` +
             `</div>`
         
